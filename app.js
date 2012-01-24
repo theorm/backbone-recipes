7e097d2a3e84a6b9729d528541ba5967b8a62054
theorm <theorm@gmail.com>
2012-01-24 17:40:14 +1000

    Google news pagination added. Plus simple flash message view.

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 var App = (function() {
     
+    // Application wide ajax error handler
+    $(document).ajaxError(function myErrorHandler(event, xhr, ajaxOptions, thrownError) {
+		var model;
+		try {
+			var data = JSON.parse(xhr.responseText);
+			model = new Backbone.Model(data);
+		} catch(e) {
+			if (xhr.responseText != undefined && xhr.responseText != '') {
+				text = xhr.responseText;
+    			model = new Backbone.Model({'text' : text});
+			} else {
+			    console.dir(xhr);
+    			model = new Backbone.Model({'text' : 'Unknown problem. See console log for details.'});			    
+			}
+		}
+		
+		var text = 'An error occured';
+		if (xhr.responseText != undefined && xhr.responseText != '')
+			text = xhr.responseText;
+        var msg = new Recipes.Views.FlashMessage({
+            el: $("#messages"),
+            model: model,
+            template: _.template('<div class="alert-message error"><p><%= text %></p></div>')
+        });
+        msg.render();
+	});
+	
+    
     var App = {
         Models: {},
         Collections: {},
@@ -14,15 +42,16 @@ var App = (function() {
     App.Routers.Examples = Backbone.Router.extend({
         routes: {
             "" : "home",
-            "pagination" : "pagination",
+            "twitter-pagination" : "twitter_pagination",
+            "google-pagination" : "google_pagination",
         },
         home: function() {
             this.enable('');
             var tmpl = $("#home").html();
             $("#main").empty().append(_.template(tmpl));
         },
-        pagination: function() {
-            this.enable('pagination');
+        twitter_pagination: function() {
+            this.enable('twitter-pagination');
             var tmpl = $("#pagination").html();
             $("#main").empty().append(_.template(tmpl));
             
@@ -31,6 +60,27 @@ var App = (function() {
                 el: $("#table"),
                 collection: collection,
                 row_template: _.template("<td><img src='<%= profile_image_url %>'></img><br/>@<%= from_user %></td><td><%= text %></td>")
+            });
+            new Recipes.Views.Pager({
+                el: $("#pager"),
+                collection: collection
+            });
+            var search = new Recipes.Views.SearchBox({
+                el: $("#search"),
+                collection: collection
+            })
+            search.$("input").val("backbone.js").trigger("keyup");
+        },
+        google_pagination: function() {
+            this.enable('google-pagination');
+            var tmpl = $("#pagination").html();
+            $("#main").empty().append(_.template(tmpl));
+            
+            var collection = new App.Collections.GoogleNewsSearch();
+            new Recipes.Views.Table({
+                el: $("#table"),
+                collection: collection,
+                row_template: _.template("<td><%= publisher %></td><td><%= title %></td><td><%= content %></td>")
             });
             new Recipes.Views.Pager({
                 el: $("#pager"),
@@ -68,6 +118,35 @@ var App = (function() {
 				options.data.rpp = this.perpage;
 			if (this.page !== undefined)
 				options.data.page = this.page;
+
+			return Backbone.Collection.prototype.fetch.call(this, options);
+		},
+		search: function(text) {
+		    this.query['q'] = text;
+		    this.fetch();
+		},
+    });
+
+    // Pagination example using Twitter search
+    App.Collections.GoogleNewsSearch = Recipes.Collections.PaginatedCollection.extend({
+        query: {},
+        url: 'https://ajax.googleapis.com/ajax/services/search/news',
+        perpage: 4,
+        page: 1,
+    	parse: function(resp,xhr) {
+			this.page = resp.responseData.cursor.currentPageIndex + 1;
+			this.total = resp.responseData.cursor.estimatedResultCount < 32 ? resp.responseData.cursor.estimatedResultCount : 32;
+			return resp.responseData.results;
+		},
+		fetch : function(options) {
+			options || (options = {});
+			options.data || (options.data = {});
+			options.data = _.extend(options.data,this.query)
+			
+			options.data.v = '1.0';
+			options.data.rsz = this.perpage;
+			if (this.page !== undefined)
+				options.data.start = (this.page-1) * this.perpage;
 
 			return Backbone.Collection.prototype.fetch.call(this, options);
 		},
---
+++ js/messages.js
@@ -0,0 +1,35 @@
+var Recipes = (function(Recipes) {
+    Recipes.Views || (Recipes.Views = {});    
+
+    Recipes.Views.FlashMessage = Backbone.View.extend({
+		initialize: function() {
+			if (this.template === undefined && this.options.template === undefined) {
+				throw '"template" option is missing';
+			}
+			if (this.options.template != undefined) {
+			    this.template = this.options.template;
+			}
+			if (this.options.timeout != undefined) {
+			    this.timeout = this.options.timeout;
+			} else {
+    			this.timeout = 3000;			    
+			}
+		},
+		render: function() {
+		    var message = $(this.template(this.model.toJSON()));
+		    message.css("display","none");
+		    this.el.empty().append(message);
+		    var timeout = this.timeout;
+		    message.fadeIn('slow',function() {
+		        setTimeout(function() {
+    		        message.fadeOut('slow',function() {
+    		           message.remove(); 
+    		        });		            
+		        },timeout);
+		    });
+		},
+	});
+
+
+	return Recipes;
+})(Recipes || {});
