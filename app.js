c856cb6efae3bf96a8b525d9f0b2a9d13f3fd63c
theorm <theorm@gmail.com>
2013-09-01 18:43:46 +1000

    - added foursquare search

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var App = (function() {
         var msg = new Recipes.Views.FlashMessage({
             el: $("#messages"),
             model: model,
-            template: _.template('<div class="alert-message error"><p><%= text %></p></div>')
+            template: _.template('<div class="alert alert-danger"><p><%= text || "" %></p></div>')
         });
         msg.render();
 	});
@@ -42,13 +42,35 @@ var App = (function() {
     App.Routers.Examples = Backbone.Router.extend({
         routes: {
             "" : "home",
-            "twitter-pagination" : "twitter_pagination",
+            "foursquare-pagination": "foursquare_pagination",
+            // "twitter-pagination" : "twitter_pagination",
             "google-pagination" : "google_pagination",
         },
         home: function() {
             this.enable('');
             var tmpl = $("#home").html();
             $("#main").empty().append(_.template(tmpl));
+        },
+        foursquare_pagination: function() {
+            this.enable('foursquare-pagination');            
+            var tmpl = $("#pagination").html();
+            $("#main").empty().append(_.template(tmpl));
+            
+            var collection = new App.Collections.FoursquareSearch();
+            new Recipes.Views.Table({
+                el: $("#table"),
+                collection: collection,
+                row_template: _.template("<td><% try { %><img src='<%= photo.prefix %>100x100<%= photo.suffix %>'></img><% } catch(e) {} %></td><td><%= venue.name %></td>")
+            });
+            new Recipes.Views.Pager({
+                el: $("#pager"),
+                collection: collection
+            });
+            var search = new Recipes.Views.SearchBox({
+                el: $("#search"),
+                collection: collection
+            });
+            search.$("input").val("Brisbane, AU").trigger("keyup");
         },
         twitter_pagination: function() {
             this.enable('twitter-pagination');
@@ -97,6 +119,39 @@ var App = (function() {
             $("#topbar a[href='#" + section + "']").parent("li").addClass("active");
         }
     });
+
+    App.Collections.FoursquareSearch = Recipes.Collections.PaginatedCollection.extend({
+        query: {
+            locale: 'en',
+            v: '20130830',
+            limit: 30,
+            section: 'coffee',
+            near: 'Brisbane, AU',
+            oauth_token: 'QEJ4AQPTMMNB413HGNZ5YDMJSHTOHZHMLZCAQCCLXIX41OMP'  // Thank you Foursquare for exposing your public token                     
+        },
+        perpage: 30,
+        page: 1,
+        url: 'https://api.foursquare.com/v2/venues/explore',
+        parse: function(resp, xhr) {
+            this.total = parseInt(resp.response.totalResults) || 0;
+            return resp.response.groups[0].items;
+        },
+        fetch: function(options) {
+            options || (options = {});
+            options.data || (options.data = {});
+            options.data = _.extend(options.data,this.query);
+
+            if (this.page != undefined)
+                options.data.offset = (this.page-1) * this.perpage;
+            options.reset = true;
+
+            return Backbone.Collection.prototype.fetch.call(this, options);
+        },
+        search: function(text) {
+            this.query['near'] = text;
+            this.fetch();
+        }
+    });
     
     // Pagination example using Twitter search
     App.Collections.TwitterSearch = Recipes.Collections.PaginatedCollection.extend({
@@ -118,6 +173,7 @@ var App = (function() {
 				options.data.rpp = this.perpage;
 			if (this.page !== undefined)
 				options.data.page = this.page;
+            options.reset = true;
 
 			return Backbone.Collection.prototype.fetch.call(this, options);
 		},
@@ -141,6 +197,7 @@ var App = (function() {
                 this.page = 1;
                 this.total = 1;
             }
+            console.log(resp.responseData.results)
 			return resp.responseData.results;
 		},
 		fetch : function(options) {
